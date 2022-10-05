de5e3080461e7c5b848645a863cd8c6d707635d5
Liubov <lyubasik85@rambler.ru>
2022-10-05 17:40:41 +0300

    Показать вторую цифру трехзначного числа

---
+++ Zadacha_7/Program.cs
@@ -0,0 +1,8 @@
+﻿// Показать вторую цифру трёхзначного числа
+
+Console.Clear();
+
+int number = new Random().Next(100, 999);
+int result = number / 10 % 10;
+Console.WriteLine(number);
+Console.WriteLine(result);
