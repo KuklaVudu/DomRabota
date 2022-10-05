3945a222f1ef87aacac548c57a5240e504345654
Liubov <lyubasik85@rambler.ru>
2022-10-05 17:29:54 +0300

    Выяснить является ли число четным

---
+++ Zadacha_4/Program.cs
@@ -0,0 +1,16 @@
+﻿// Выяснить является ли число чётным
+
+Console.Clear();
+
+int a;
+Console.WriteLine("Введите число: ");
+a = int.Parse(Console.ReadLine() ?? "0");
+if (a % 2 == 0)
+{
+    Console.WriteLine("Число четное");
+}
+else
+{
+    Console.WriteLine("Число нечетное");
+}
+Console.WriteLine();
